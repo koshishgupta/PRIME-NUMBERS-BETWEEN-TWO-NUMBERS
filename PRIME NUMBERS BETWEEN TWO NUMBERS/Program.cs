c32f7828a02f77baeeb0ff4664e8e55297d8ef72
koshishgupta <guptakoshish5@gmail.com>
2024-01-19 17:39:34 +0530

    Add project files.

---
+++ PRIME NUMBERS BETWEEN TWO NUMBERS/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PRIME_NUMBERS_BETWEEN_TWO_NUMBERS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, i, j;
+            Console.WriteLine("ENTER A TWO NUMBER  ");
+             a=Convert.ToInt32(Console.ReadLine());
+             b=Convert.ToInt32(Console.ReadLine());
+            for( i = a; i <= b; i++)
+            {
+                for( j=2;j <= b; j++)
+                {
+                    if (i % j == 0)
+                        break;
+                }
+       
+                if (i==j)
+                
+                    Console.WriteLine(j);
+                
+            }
+        }
+    }
+}
